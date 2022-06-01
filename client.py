a2e9a797a72a5876d83eb05fe252efb74ea85c10
Lorena Mamede <lorenamb@dcc.ufrj.br>
2022-05-31 21:45:05 -0300

    Criando base do projeto
    Implementando funcionalidade de login básica sem validações no cliente e no servidor.
    Criando template para os outros comandos.

---
+++ client.py
@@ -0,0 +1,85 @@
+#servidor de echo: lado cliente
+import socket
+import json
+import select
+import sys
+
+CENTRAL_SERVER='localhost'
+CENTRAL_SERVER_PORT = 5000
+
+HOST = ''
+PORT = 5001
+inputs = [sys.stdin]
+
+connections = {}
+threads = []
+
+def iniciaCliente():
+    '''Cria um socket de servidor para 
+    atender as requisicoes de conversa
+    dos outros clientes.
+    Saida: socket criado'''
+    
+    # cria socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Internet (IPv4 + TCP) 
+
+    sock.bind((HOST,PORT))
+
+    sock.setblocking(False)
+
+    inputs.append(sock)
+    
+    return sock
+
+def connectWithCentralServer():
+    newSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Internet (IPv4 + TCP) 
+    newSock.connect((CENTRAL_SERVER, CENTRAL_SERVER_PORT))
+    
+    return newSock
+
+def login():
+    serverSock = connectWithCentralServer()
+
+    msg = input("Escolha um username: ('fim' para terminar):")
+    #if msg == 'fim': break 
+
+    # envia a mensagem do usuario para o servidor
+    mensagem = {"operacao":"login", "username":msg, "porta": PORT}
+    mensagemJson = json.dumps(mensagem)
+    serverSock.send(mensagemJson.encode("utf-8"))
+
+    #TODO: tratar tamanho da mensagem, tratar se usuario ja existe
+    #msg = sock.recv(1024) 
+
+    # imprime a mensagem recebida
+    #print(str(msg, encoding='utf-8'))
+
+
+def main():
+    '''Funcao principal do cliente'''
+    #inicia o cliente
+    sock = iniciaCliente()
+    
+    r,w,x = select.select(inputs, [], [])
+    for request in r:
+        if request == sock:
+            #outro cliente iniciando conversa
+            #servidor respondendo ou cliente conversando
+            #criar thread
+            pass
+        
+        elif request == sys.stdin:
+            cmd = input()
+            if cmd == 'login':
+                login()
+            elif cmd == 'logoff':
+                #remove registro do servidor
+                pass
+            elif cmd == 'get_lista':
+                #recupera listagem com usuarios ativos
+                pass
+            else:
+                #envio de mensagem 
+                print(cmd)
+
+main()
---
+++ server.py
@@ -0,0 +1,107 @@
+import socket
+import select
+import sys
+import threading
+import json
+
+# define a localizacao do servidor
+HOST = '' # vazio indica que podera receber requisicoes a partir de qq interface de rede da maquina
+PORT = 5000 # porta de acesso
+
+#define a lista de I/O de interesse (jah inclui a entrada padrao)
+entradas = [sys.stdin]
+#armazena historico de conexoes 
+conexoes = {}
+usuarios = {}
+
+def iniciaServidor():
+    '''Cria um socket de servidor e o coloca em modo de espera por conexoes
+    Saida: o socket criado'''
+    # cria o socket 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Internet( IPv4 + TCP) 
+
+    # vincula a localizacao do servidor
+    sock.bind((HOST, PORT))
+
+    # coloca-se em modo de espera por conexoes
+    sock.listen(5) 
+
+    # configura o socket para o modo nao-bloqueante
+    sock.setblocking(False)
+
+    # inclui o socket principal na lista de entradas de interesse
+    entradas.append(sock)
+
+    return sock
+
+def aceitaConexao(sock):
+    '''Aceita o pedido de conexao de um cliente
+    Entrada: o socket do servidor
+    Saida: o novo socket da conexao e o endereco do cliente'''
+
+    # estabelece conexao com o proximo cliente
+    clisock, endr = sock.accept()
+
+    # registra a nova conexao
+    conexoes[clisock] = endr 
+
+    return clisock, endr
+
+def atendeRequisicoes(clisock, endr):
+    '''Recebe mensagens e as envia de volta para o cliente (ate o cliente finalizar)
+    Entrada: socket da conexao e endereco do cliente
+    Saida: '''
+
+    while True:
+        #recebe dados do cliente
+        data = clisock.recv(1024) 
+        if not data: # dados vazios: cliente encerrou
+            print(str(endr) + '-> encerrou')
+            clisock.close() # encerra a conexao com o cliente
+            return 
+        
+        data = json.loads(data.decode("utf-8"))
+        operacao = data["operacao"]
+
+        if operacao == 'login':
+            login(data["username"], endr, data["porta"])
+        elif operacao == 'logoff':
+            #remove registro do servidor
+            pass
+        elif operacao == 'get_lista':
+            #recupera listagem com usuarios ativos
+            pass
+
+def login(username, endr, porta):
+    #TODO: validacao de usuario
+    usuarios[username] = {"endereco": endr[0], "porta": porta}
+    print("Novo usuario: ", data["username"])
+
+def main():
+    '''Inicializa e implementa o loop principal (infinito) do servidor'''
+    clientes=[] #armazena as threads criadas para fazer join
+    sock = iniciaServidor()
+    print("Pronto para receber conexoes...")
+    while True:
+        #espera por qualquer entrada de interesse
+        leitura, escrita, excecao = select.select(entradas, [], [])
+        #tratar todas as entradas prontas
+        for pronto in leitura:
+            if pronto == sock:  #pedido novo de conexao
+                clisock, endr = aceitaConexao(sock)
+                print ('Conectado com: ', endr)
+                #cria nova thread para atender o cliente
+                cliente = threading.Thread(target=atendeRequisicoes, args=(clisock,endr))
+                cliente.start()
+                clientes.append(cliente) #armazena a referencia da thread para usar com join()
+            elif pronto == sys.stdin: #entrada padrao
+                cmd = input()
+                if cmd == 'fim': #solicitacao de finalizacao do servidor
+                    for c in clientes: #aguarda todas as threads terminarem
+                        c.join()
+                    sock.close()
+                    sys.exit()
+                elif cmd == 'hist': #outro exemplo de comando para o servidor
+                    print(str(conexoes.values()))
+
+main()
